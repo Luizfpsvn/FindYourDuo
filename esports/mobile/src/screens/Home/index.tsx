edaefbd85f4852c5dd3a388c38877733d2d27a0e
Luiz Fernando <luizfernando.cg@hotmail.com>
2022-09-13 20:28:48 -0300

    Created GameCard Component to use in Home page

--- esports/mobile/src/screens/Home/index.tsx
+++ esports/mobile/src/screens/Home/index.tsx
@@ -1,15 +1,26 @@
-import React from "react";
-
 import { View, Image } from "react-native";
 
 import { styles } from "./styles";
 
 import logoImg from "../../assets/logo-nlw-esports.png";
 
+import { GAMES } from "../../utils/games";
+
+import { Heading } from "../../components/Heading";
+
+import { GameCard } from "../../components/GameCard";
+
 export function Home() {
   return (
     <View style={styles.container}>
       <Image source={logoImg} style={styles.logo} />
+
+      <Heading
+        title="Encontre o seu duo!"
+        subtitle="Selecione o game que deseja jogar..."
+      />
+
+      <GameCard data={GAMES[0]} />
     </View>
   );
 }
