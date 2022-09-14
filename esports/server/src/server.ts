f95878308d558a4488a5feaf2bf18d7cff05d150
Luiz Fernando <luizfernando.cg@hotmail.com>
2022-09-14 11:17:29 -0300

    Install Prisma ORM to handle the DB, and begin configuration of the enviroment

--- esports/server/src/server.ts
+++ esports/server/src/server.ts
@@ -3,14 +3,32 @@ import express from "express"; //Begin project with the new importation type: mo
 const app = express();
 app.use(express.json());
 
-app.get("/ads", (request, response) => {
+// HTTP methods / API Restful
+
+app.get("/games", (request, response) => {
+  return response.json([]);
+});
+
+app.post("/ads", (request, response) => {
+  return response.status(201).json(["ads"]);
+});
+
+app.get("/games/:id/ads", (request, response) => {
+  // const gameId = request.params.id;
+
   return response.json([
     { id: 1, name: "Anúncio 1" },
     { id: 2, name: "Anúncio 2" },
     { id: 3, name: "Anúncio 3" },
     { id: 4, name: "Anúncio 4" },
-    { id: 5, name: "Anúncio 5" },
+    { id: 5, name: "Anúncio 5" }
   ]);
+});
+
+app.get("/ads/:id/discord", (request, response) => {
+  const adId = request.params.id;
+
+  return response.send(adId);
 });
 
 const PORT = 3333;
